6fb58101c622c7c47a1f2bb9a83ccb4a29e4c043
Parth Magar <parthmagar789@gmail.com>
2026-03-16 20:13:04 +0530

    starting with backend server

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1>Parth Magar</h1>");
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port no 4000 successfully');
+    
+})
