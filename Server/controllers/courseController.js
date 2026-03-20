ea3bae5ea29d7fc1ccc15c2280ce0a34357f718d
Parth Magar <parthmagar789@gmail.com>
2026-03-20 16:22:07 +0530

    get all courses & get course details controller done

--- Server/controllers/courseController.js
+++ Server/controllers/courseController.js
@@ -73,3 +73,82 @@ exports.createCourse = async (req, res) => {
     }
 }
  
+// get all courses handler function
+exports.getAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.find(
+            {},
+            {
+                courseName: true,
+                price: true,
+                thumbnail: true,
+                instructor: true,
+                ratingAndReviews: true,
+                studentsEnroled: true,
+            }
+        ).populate('instructor').exec();
+
+        return res.status(200).json({
+            success: true,
+            message: 'All courses fetched successfully!',
+            data:courses
+        })
+    }catch(err) {
+        console.log('Error in getting all courses: ', err);
+        
+        return res.status(500).json({
+            success: false,
+            message: 'Error While Getting All Courses'
+        })
+    }
+}
+
+exports.getCourseDetails = async (req, res) => {
+    try {
+        const { courseId } = req.body;
+        
+        if(!courseId) {
+            return res.status(400).json({
+                success: false,
+                message: 'course id is required!',
+            })
+        }
+
+        const course = await Course.findById(courseId)
+        .populate({
+            path: instructor,
+            populate: {
+                path: 'additionalDetails',
+            }
+        })
+        .populate('category')
+        .populate('ratingAndReviews')
+        .populate({
+            path: 'courseContent',
+            poulate: {
+                path: 'subSection',
+            }
+        })
+        .exec();
+
+        if(!course) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid course id',
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Course Details Fetched Successfully!',
+            data: course
+        })
+    }catch(err) {
+        console.log('Get course details error: ',err);
+        
+        return res.status(500).json({
+            success: false,
+            message: 'Error while getting course details'
+        })
+    }
+}
