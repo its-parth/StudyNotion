98d6a6606ba3e2d75d7e322933023b7ca7e5ea9b
Parth Magar <parthmagar789@gmail.com>
2026-03-25 15:06:21 +0530

    index.js done starting testing the code

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,48 @@
 const express = require('express');
+const { connectDB } = require('./config/database');
+const cookieparser = require('cookie-parser');
+const { cloudinaryConnect } = require('./config/cloudinary');
+const fileupload = require('express-fileupload');
+
+const userRoutes = require('./routes/userRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+
+const cors = require('cors');
 
 const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(fileupload(
+    {
+        useTempFiles: true,
+        tempFileDir: '/tmp/'
+    }
+));
+app.use(cookieparser());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+)
+connectDB();
+cloudinaryConnect();
+
+
+// adding routes mapping
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/course', courseRoutes);
+app.use('/api/v1/profile', profileRoutes);
+
+
 app.get('/', (req, res) => {
     res.send("<h1>Parth Magar</h1>");
 });
 
-app.listen(4000, () => {
-    console.log('Server is running on port no 4000 successfully');
+app.listen(PORT, () => {
+    console.log('Server is running on port no: ',PORT);
 });
